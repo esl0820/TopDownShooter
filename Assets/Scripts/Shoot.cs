6a0a558ea5b69e61680619d4e1f9477f4a35778e
esl0820 <esl361@nyu.edu>
2017-01-30 11:49:32 -0500

    First Push
    Let's take everything here, and push it over there

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject stinger;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fire ();
+
+		
+	}
+
+	void Fire () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate(stinger, transform.position, transform.rotation);
+		}
+	}
+}
