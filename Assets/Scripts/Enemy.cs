91c96df775ff388ce9d307a2e5bbda74ac7e43b4
esl0820 <esl361@nyu.edu>
2017-01-30 11:49:32 -0500

    Blerb
    added enemy code with sandboxi

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Health : MonoBehaviour {
+
+	public float speed;
+	float t = 0f;
+	float rotate;
+	public float vision;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+
+		if (this.tag == "Beetle") {
+			Beetle ();
+		}
+		if (this.tag == "Mosquito") {
+			Mosquito ();
+		}
+		
+	}
+
+	void Beetle () {
+		rotate = Random.Range (-90, 90);
+
+		var fwd = transform.TransformDirection (Vector3.forward);
+
+		if (Physics.Raycast (transform.position, fwd, vision)) {
+			transform.Rotate (0, rotate, 0);
+		}
+	}
+
+	void Mosquito () {
+		transform.position.x = 3f * Mathf.Cos(t);
+		transform.position.y += speed * Time.deltaTime;
+
+		t += Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 1f;
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement();
+		
+	}
+
+	void Movement () {
+		//Move Forwards and Backwards
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, 0, speed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, 0, -speed/3);
+		}
+
+		//Turn Left and Right
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0, rotationSpeed, 0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (0, -rotationSpeed, 0);
+		}
+	}
+}
